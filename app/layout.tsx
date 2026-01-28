550b93c9ce98039739f7a5a6afcf92606d9f02a9
Neel Joshi <nbjoshi@unc.edu>
2026-01-28 17:52:54 -0500

    analytics comp

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Navbar } from '@/components/site/Navbar';
 import { Footer } from '@/components/site/Footer';
 import { ThemeProvider } from '@/components/theme/theme-provider';
+import { Analytics } from "@vercel/analytics/next"
 import { Newsreader } from 'next/font/google';
 import '@/styles/globals.css';
 
@@ -36,6 +37,7 @@ export default function RootLayout({
             <Navbar />
             <main className="flex-1">{children}</main>
             <Footer />
+            <Analytics />
           </div>
         </ThemeProvider>
       </body>
